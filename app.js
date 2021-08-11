b1739c57255df4afc322983b4a9f43ac4de01204
asaqa988 <asaqa001@gmail.com>
2021-08-11 12:22:41 +0300

    exam

---
+++ app.js
@@ -0,0 +1,57 @@
+'use strict'
+
+
+function Donor(Name, Amount) {
+
+    donorName = this.Name
+    donortAmount = this.Amount
+    age = Math.floor((Math.random() * 60) + 20);
+
+
+}
+
+
+let donPart = [];
+
+
+donPart.push($this[Name],  $this[Amount], age)
+
+console.log (donPart)
+
+
+let donor1 = new Donor()
+
+
+
+
+
+
+
+for (let i = 0; i < donPart.length; i++) {
+
+
+    let table1 = document.getElementById('tab1')
+    let table1Tr = document.createElement('tr')
+
+    table1.appendChild.table1Tr 
+
+
+    let tabel1Tr2 = document.createElement('tr')
+    table1.appendChild.table2Tr
+
+}
+
+
+
+let itemsArray = []
+
+itemsArray.push ($this.donorName , $this.donortAmount,this.age)
+// localStorage.setItem('items','values')
+
+
+
+localStorage.setItem('items', JSON.stringify(itemsArray))
+const data = JSON.parse(localStorage.getItem('items'))
+
+
+
